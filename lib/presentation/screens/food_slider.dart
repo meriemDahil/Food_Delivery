7c8fae8654f51fd4a42e1387e4024feaccbbe2e7
meriem-da <meriemdahil31@gmail.com>
2024-05-27 19:42:03 +0200

    zoom in zoom out pageview scaling

--- lib/presentation/screens/food_slider.dart
+++ lib/presentation/screens/food_slider.dart
@@ -65,6 +65,17 @@ class _FoodSliderState extends State<FoodSlider> {
        var currTrans= _height*(1-currScale)/2;
        matrix =Matrix4.diagonal3Values(1, currScale, 1);
        matrix =Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans , 0);
+
+    }else if(index==_currentPageValue.floor()-1){
+       var currScale =1-(_currentPageValue-index)*(1-_scaleFactor);
+       var currTrans= _height*(1-currScale)/2;
+       matrix =Matrix4.diagonal3Values(1, currScale, 1);
+       matrix =Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans , 0);
+
+    }
+    else{
+      var currScale =0.8;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, _height*(1-_scaleFactor)/2, 1);
     }
     return Transform(
       transform: matrix,
